8e1927f57b0666e288ae1c8052743c7cbb4522e4
KATO Hayate <dev@hayatek.jp>
2025-05-25 23:33:05 +0900

    Initial commit

---
+++ src/styles/font.ts
@@ -0,0 +1,8 @@
+import { Roboto_Slab } from 'next/font/google';
+
+export const RobotoSlab = Roboto_Slab({
+  variable: '--font-roboto-slab',
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+});
