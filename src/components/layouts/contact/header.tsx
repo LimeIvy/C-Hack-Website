39f1f027adb2153ea1ac6cd0b26c42c0f0281bf0
KATO Hayate <dev@hayatek.jp>
2025-07-20 04:35:22 +0900

    fix(ui): スマホでの表示崩れを調整
    強引な手段なので将来的に修正する

--- src/components/layouts/contact/header.tsx
+++ src/components/layouts/contact/header.tsx
@@ -11,7 +11,7 @@ import type { JSX } from 'react';
 export default function Header(): JSX.Element {
   return (
     <div className="container mx-auto px-4 pb-4 mt-4 md:mt-8 item-center justify-center flex">
-      <Link href="/" className="flex items-center space-x-2">
+      <Link href="/" className="ml-[35px] md:pl-0 flex items-center space-x-2">
         <Code className="h-16 w-16 text-emerald-400" />
         <Image src={LogoImage} alt="ロゴ画像" height={256} className="ml-[-70px]" />
       </Link>
