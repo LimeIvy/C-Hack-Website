60574e53a42f109b0a22d4468c1c16f000e7c652
KATO Hayate <dev@hayatek.jp>
2025-07-20 16:47:46 +0900

    add: Analyticsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 import '@/styles/globals.css';
 
 import { IBMPlexSansJP, NunitoSans } from '@/styles/font';
@@ -19,6 +21,7 @@ export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
       <body className={`${NunitoSans.className} ${IBMPlexSansJP.className} relative min-h-screen`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-3N7LJC8NB4" />
     </html>
   );
 }
